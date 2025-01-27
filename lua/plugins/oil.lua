b2ff13ea0258ebd2ef8418fc800543121f87e44e
r4ravi2008 <r4ravi2008@gmail.com>
2025-01-27 12:58:14 -0800

    feat: add Oil plugin with configuration and keybindings

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,16 @@
+return {
+  "stevearc/oil.nvim",
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  opts = {
+    view_options = {
+      show_hidden = true,
+    },
+  },
+  -- Optional dependencies
+  dependencies = { { "echasnovski/mini.icons", opts = {} } },
+  keys = {
+    -- suggested keymap
+    { "<leader>fo", "<cmd>Oil<cr>", desc = "Open Oil" },
+  },
+}

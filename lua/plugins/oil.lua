9b253bbbf235a117dfe52c4f9b5a32e9c02879ba
r4ravi2008 <r4ravi2008@gmail.com>
2025-07-19 23:00:31 -0700

    updated

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,17 +1,32 @@
 return {
   "stevearc/oil.nvim",
-  ---@module 'oil'
-  ---@type oil.SetupOpts
-  vscode = true,
-  opts = {
-    view_options = {
-      show_hidden = true,
+  lazy = false, -- Recommended not to lazy load for performance
+  dependencies = { "nvim-tree/nvim-web-devicons" }, -- For icons
+
+  keys = {
+    -- suggested keymap to open oil
+    { "<leader>fo", "<cmd>Oil<cr>", desc = "Open Oil" },
+    {
+      "yp",
+      function()
+        require("oil.actions").copy_entry_path.callback()
+        vim.fn.setreg("+", vim.fn.getreg(vim.v.register))
+      end,
+      mode = "n",
+      desc = "Copy filepath to system clipboard (Oil)",
+      buffer = true, -- Ensure this keymap only applies in Oil buffer
+    },
+    {
+      "<leader>%",
+      ":let @+=expand('%:p')<CR>",
+      mode = "n",
+      desc = "Copy current file path",
     },
   },
-  -- Optional dependencies
-  dependencies = { { "echasnovski/mini.icons", opts = {} } },
-  keys = {
-    -- suggested keymap
-    { "<leader>fo", "<cmd>Oil<cr>", desc = "Open Oil" },
+
+  opts = {
+    view_options = {
+      show_hidden = true, -- Show hidden files
+    },
   },
 }

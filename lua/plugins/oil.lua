298ab4a561528c40191440f2e86e2d8d8e29830a
r4ravi2008 <r4ravi2008@gmail.com>
2025-10-17 15:24:33 -0700

    fix oil vim tmux navigation issue

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,21 +1,11 @@
 return {
   "stevearc/oil.nvim",
-  lazy = false, -- Recommended not to lazy load for performance
-  dependencies = { "nvim-tree/nvim-web-devicons" }, -- For icons
+  lazy = false,
+  dependencies = { "nvim-tree/nvim-web-devicons" },
 
+  -- Global (non-Oil) keymaps
   keys = {
-    -- suggested keymap to open oil
     { "<leader>fo", "<cmd>Oil<cr>", desc = "Open Oil" },
-    {
-      "yp",
-      function()
-        require("oil.actions").copy_entry_path.callback()
-        vim.fn.setreg("+", vim.fn.getreg(vim.v.register))
-      end,
-      mode = "n",
-      desc = "Copy filepath to system clipboard (Oil)",
-      buffer = true, -- Ensure this keymap only applies in Oil buffer
-    },
     {
       "<leader>%",
       ":let @+=expand('%:p')<CR>",
@@ -24,9 +14,31 @@ return {
     },
   },
 
+  -- Make Oil leave <C-hjkl> alone so tmux navigator can work
   opts = {
+    keymaps = {
+      ["<C-h>"] = false,
+      ["<C-j>"] = false,
+      ["<C-k>"] = false,
+      ["<C-l>"] = false,
+      -- optional: give Oil a different refresh key so you still have one
+      ["R"] = "actions.refresh",
+    },
     view_options = {
-      show_hidden = true, -- Show hidden files
+      show_hidden = true,
     },
   },
+
+  -- Put Oil-only maps behind a FileType autocmd so they’re buffer-local
+  init = function()
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "oil",
+      callback = function(ev)
+        vim.keymap.set("n", "<leader>fyp", function()
+          require("oil.actions").copy_entry_path.callback()
+          vim.fn.setreg("+", vim.fn.getreg(vim.v.register))
+        end, { buffer = ev.buf, desc = "Copy filepath to system clipboard (Oil)" })
+      end,
+    })
+  end,
 }
